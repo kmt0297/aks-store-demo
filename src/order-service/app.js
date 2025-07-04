86965a56fc485a3d79b3354d17f9fa374ddeca42
kmt0297 <49325344+kmt0297@users.noreply.github.com>
2025-07-04 12:20:10 -0500

    Update app.js
    added ability to use mongodb

--- src/order-service/app.js
+++ src/order-service/app.js
@@ -2,10 +2,28 @@
 
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
+const { MongoClient } = require('mongodb')
 
 module.exports = async function (fastify, opts) {
 // Place here your custom code!
 
+  // Connect to MongoDB if MONGO_URL is set
+  const mongoUrl = process.env.MONGO_URL
+  if (mongoUrl) {
+    try {
+      const client = await MongoClient.connect(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      fastify.log.info('Connected to MongoDB from order-service')
+      client.close()
+    } catch (err) {
+      fastify.log.error('Failed to connect to MongoDB:', err.message)
+    }
+  } else {
+    fastify.log.warn('MONGO_URL not set in environment variables')
+  }
+  
   fastify.register(require('@fastify/cors'), {
     origin: '*'
   })
